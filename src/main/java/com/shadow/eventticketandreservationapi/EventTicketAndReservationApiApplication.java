f86549532f66fdb42b0b452219b1b0050bc408a2
Raunit Raj <Raunit-7549@users.noreply.github.com>
2026-09-24 13:56:40 +0000

    Project file created

---
+++ src/main/java/com/shadow/eventticketandreservationapi/EventTicketAndReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.shadow.eventticketandreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketAndReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketAndReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shadow/eventticketandreservationapi/EventTicketAndReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.eventticketandreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketAndReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
