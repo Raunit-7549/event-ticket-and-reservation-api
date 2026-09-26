555f7ea94153616d88ba929910aa94033f5bb5e1
Raunit Raj <Raunit-7549@users.noreply.github.com>
2026-09-26 01:42:40 +0000

    created the controller for events

--- src/main/java/com/shadow/eventticketandreservationapi/controller/EventController.java
+++ src/main/java/com/shadow/eventticketandreservationapi/controller/EventController.java
@@ -3,19 +3,45 @@ package com.shadow.eventticketandreservationapi.controller;
 import com.shadow.eventticketandreservationapi.model.Event;
 import com.shadow.eventticketandreservationapi.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("events")
 public class EventController {
 
     @Autowired
     EventService service;
 
-    @GetMapping("/activeEvents")
+    @GetMapping
     public List<Event> getAllActiveEvents(){
         return service.getAllActiveEvents();
     }
+
+    @GetMapping("/{id}")
+    public Event getEventDetailsByID(@PathVariable int id){
+        return service.getEventDetailsByID(id);
+    }
+
+    @GetMapping("/venue/{venue}")
+    public List<Event> getEventDetailsByID(@PathVariable String venue){
+        return service.getEventsByVenue(venue);
+    }
+
+    @PostMapping
+    public void createEvent(@RequestBody Event event){
+        service.createEvent(event);
+    }
+
+    @PutMapping("/{id}")
+    public void updateEvent(@RequestBody Event event, @PathVariable int id){
+        service.updateEvent(event, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void cancelEvent(@PathVariable int id){
+        service.cancelEvent(id);
+    }
+
 }
